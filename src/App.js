4b5c760f721662c7a968a2877e962f5e978c8ae2
nerocui <nerocui@outlook.com>
2021-10-01 18:17:18 -0700

    added protocol verification, delete button

--- src/App.js
+++ src/App.js
@@ -2,44 +2,79 @@ import { useCallback, useMemo, useState } from 'react';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
+import QrScanner from 'qr-scanner';
 import './App.css';
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import QrScannerWorkerPath from '!!file-loader!../node_modules/qr-scanner/qr-scanner-worker.min';
+
+QrScanner.WORKER_PATH = QrScannerWorkerPath;
 
 const Input = styled('input')({
   display: 'none',
 });
 
 const App = () => {
-  const existingFile = useMemo(() => {
-    return localStorage.getItem('imagePath');
-  }, []);
+  const existingFile = localStorage.getItem('imagePath');
   const [img, setImg] = useState(existingFile);
+  const [errorString, setErrorString] = useState();
   const reader = useMemo(() => {
     const res = new FileReader();
     res.onload = function() {
-      setImg(res.result);
-      localStorage.setItem('imagePath', res.result);
+      QrScanner.scanImage(res.result)
+        .then(code => {
+          if (code.startsWith('shc:')) {
+            setImg(res.result);
+            localStorage.setItem('imagePath', res.result);
+          } else {
+            setErrorString('Invalid QR Code');
+          }
+        })
+        .catch(err => setErrorString('Invalid QR Code'));
     };
     return res;
-  }, [setImg]);
+  }, [setImg, setErrorString]);
+
   const onFileChange = useCallback((event) => {
     reader.readAsDataURL(event.target.files[0]);
-  }, [reader]);
+    setErrorString(undefined);
+  }, [reader, setErrorString]);
+
+  const onDelete = useCallback(() => {
+    if (!!img) {
+      // eslint-disable-next-line no-restricted-globals
+      let conf = confirm('Remove your Vaccine Card?');
+      if (conf) {
+        setImg(undefined);
+        localStorage.removeItem('imagePath');
+      }
+    }
+  }, [img, setImg]);
 
   return (
-    <Stack paddingTop='2rem' height='100%' alignItems='center' textAlign='end' justifyContent='space-between'>
+    <Stack margin='0' height='100%' alignItems='center' justifyContent='space-between'>
       <h1>Vaccine Passport</h1>
-      <Stack paddingX='1rem' marginBottom='8rem' justifyContent='center' justifySelf='center'>
+      <Stack padding='1rem' justifyContent='center'>
         {!img && 
         <label htmlFor="contained-button-file">
-          <Input onChange={onFileChange} accept="image/*" id="contained-button-file" multiple type="file" />
+          <Input onChange={onFileChange} accept="image/*" id="contained-button-file" type="file" />
           <Button variant="contained" component="span">
             Upload
           </Button>
         </label>}
-        {!!img && <img src={img} alt='covid vaccine qr code'/>}
+        <p style={{ color: 'red' }}>{errorString}</p>
+        {!!img && 
+          <Stack spacing='1rem'>
+            <img src={img} alt='covid vaccine qr code'/>
+            <Button
+              variant='contained'
+              color='error'
+              onClick={onDelete}>
+              Delete
+            </Button>
+          </Stack>
+        }
       </Stack>
     </Stack>
-    
   );
 }
 
